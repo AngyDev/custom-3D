cc28532c7039ea3f2f94236643c359dc453ff775
Angela Busato <busato.angela@gmail.com>
2021-09-21 10:55:29 +0200

    [FEAT] add main component and test

---
+++ src/components/Main/Main.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '@testing-library/jest-dom';
+import { mount } from 'enzyme';
+import Main from "./Main";
+
+describe('Main testing', () => {
+
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(<Main />);
+    });
+
+    it('should contain Main text', () => {
+        expect(wrapper.find('h1').text()).toContain("Main");
+    });
+
+    // it('should contain Profile component', () => {
+    //     expect(wrapper.find('Profile')).toHaveLength(1);
+    // });
+});
