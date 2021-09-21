a633361de3570031edf49a0961c2c4cee130dc2e
Angela Busato <busato.angela@gmail.com>
2021-09-21 10:55:46 +0200

    [FEAT] add components to the app file

--- src/features/App/App.js
+++ src/features/App/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Main from '../../components/Main/Main';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 export default function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Header />
+            <div className="flex">
+                <Sidebar />
+                <Main />
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./features/App/App";
+import "./assets/styles/index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
