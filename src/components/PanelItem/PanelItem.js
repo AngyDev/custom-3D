bbb556dc62cc9a243a5454ff5840fa863a6cd1cb
Angela Busato <busato.angela@gmail.com>
2021-10-08 11:42:06 +0200

    [FEAT] create buttons to handle plane close #14

--- src/components/PanelItem/PanelItem.js
+++ src/components/PanelItem/PanelItem.js
@@ -5,6 +5,7 @@ import { getScene } from '../../features/scene/sceneSlice';
 export default function PanelItem(props) {
 
     const scene = useSelector(getScene);
+    const tControls = scene.children && scene.children.find((obj) => obj.name === "TransformControls");
 
     /**
      * Adds onclick a visible/unvisible functionality
@@ -23,18 +24,35 @@ export default function PanelItem(props) {
                         item.visible = !item.visible;
                     }
                 });
+            } else if (object.type === 'Mesh') {
+                if (object.name === name) {
+                    object.visible = !object.visible;
+                }
+            }
+        })
+    }
+
+    const transformPlane = (e, mode) => {
+        const name = e.target.attributes.name.nodeValue;
+
+        scene.children.forEach((object) => {
+            if (object.name === name) {
+                tControls.attach(object);
+                tControls.setMode(mode);
             }
         })
     }
 
     return (
         <>
-        <div className="option flex">
-            <span className="visible" onClick={handleClick}></span>
-            {props.name}
-            <span id={props.uuid} name={props.name} className="delete" onClick={props.deleteClick}>
-            </span>
-        </div>
+            <div className="option flex">
+                <span className="visible" onClick={handleClick}></span>
+                {props.name}
+                {
+                    props.type === "planes" && <><span id={props.uuid} name={props.name} className="translate" onClick={(e) => transformPlane(e, "translate")}></span><span id={props.uuid} name={props.name} className="rotate" onClick={(e) => transformPlane(e, "rotate")}></span><span id={props.uuid} name={props.name} className="scale" onClick={(e) => transformPlane(e, "scale")}></span></>
+                }
+                <span id={props.uuid} name={props.name} className="delete" onClick={props.deleteClick}></span>
+            </div>
         </>
     )
 }
