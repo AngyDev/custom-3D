439e8faba83615905e3a6777c6071b6ed6fc7ed7
Angela Busato <busato.angela@gmail.com>
2021-10-11 16:05:09 +0200

    [FEAT] resize-canvas-cause-of-comments close #43

--- src/components/CommentsListPanel/CommentsListPanel.js
+++ src/components/CommentsListPanel/CommentsListPanel.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function CommentsListPanel() {
+
+    // Dispatch the resize event to recalculate the width of the canvas when the bar is open and close (return)
+    useEffect(() => {
+        window.dispatchEvent(new Event('resize'));
+
+        return () => {
+            window.dispatchEvent(new Event('resize'));
+        }
+    })
+
     return (
         <div className="commentsListPanel">
             <h3>Comments</h3>
