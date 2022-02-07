92a2f4108426f32fe0c385669bcc73b27b3a0271
Angela Busato <busato.angela@gmail.com>
2022-02-07 13:51:17 +0100

    feat: add clipping to the project close #127 close #128

---
+++ client/src/components/Clipping/Clipping.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import clipping from "../../assets/images/icons/cut-solid.svg";
+import * as THREE from "three";
+import { LogLuvEncoding, Vector3 } from "three";
+import Button from "../Button/Button";
+import { useSelector } from "react-redux";
+import { getGroup, getScene } from "../../features/scene/sceneSlice";
+
+export default function Clipping() {
+  const scene = useSelector(getScene);
+  const group = useSelector(getGroup);
+
+  const [clipped, setClipped] = useState(false);
+  const [planeLists, setPlaneLists] = useState([]);
+
+  /**
+   * Clipping of the mesh by the planes
+   */
+  const clipMesh = () => {
+    const planes = scene.children.filter((object) => object.name.startsWith("Plane"));
+    const normals = [];
+    const centers = [];
+
+    const planeList = [];
+
+    planes.forEach((item) => {
+      const plane = new THREE.Plane();
+      const normal = new THREE.Vector3();
+      const point = new THREE.Vector3();
+
+      // Gets the ceters of the planes
+      const center = getCenterPoint(item);
+      centers.push(center);
+
+      // Creates the THREE.Plane from THREE.PlaneGeometry
+      normal.set(0, 0, 1).applyQuaternion(item.quaternion);
+      point.copy(item.position);
+      plane.setFromNormalAndCoplanarPoint(normal, point);
+
+      // Saves the normals of the planes
+      normals.push(plane.normal);
+
+      planeList.push(plane);
+    });
+
+    // Calculates the barycenter of the planes
+    const pointx = centers.reduce((prev, curr) => prev + curr.x, 0) / centers.length;
+    const pointy = centers.reduce((prev, curr) => prev + curr.y, 0) / centers.length;
+    const pointz = centers.reduce((prev, curr) => prev + curr.z, 0) / centers.length;
+    const barycenter = new THREE.Vector3(pointx, pointy, pointz);
+
+    const distances = [];
+
+    // Gets the distance from the plane and the barycenter
+    planeList.forEach((item) => {
+      distances.push(item.distanceToPoint(barycenter));
+    });
+
+    // Negates only the plane with negative distance
+    distances.forEach((distance, index) => {
+      if (distance < 0) {
+        planeList[index].negate();
+      }
+    });
+
+    group.children.map((object) => {
+      if (!object.material.clippingPlanes || object.material.clippingPlanes.length === 0) {
+        object.material.clippingPlanes = planeList;
+        object.material.clipIntersection = false;
+      } else {
+        object.material.clippingPlanes = [];
+      }
+    });
+
+    setPlaneLists(planeList);
+    setClipped((prev) => !prev);
+  };
+
+  /**
+   * Gets the center of the mesh
+   * @param {Object} mesh THREE.Mesh
+   * @returns The center of the mesh
+   */
+  const getCenterPoint = (mesh) => {
+    var geometry = mesh.geometry;
+    geometry.computeBoundingBox();
+    var center = new THREE.Vector3();
+    geometry.boundingBox.getCenter(center);
+    mesh.localToWorld(center);
+    return center;
+  };
+
+  const handleNegated = () => {
+    planeLists.forEach((item) => item.negate());
+
+    group.children.map((object) => {
+      object.material.clipIntersection = !object.material.clipIntersection;
+    });
+  };
+
+  return (
+    <>
+      <Button typeClass="btn--img" img={clipping} onClick={clipMesh} />
+      {clipped && (
+        <span>
+          <input type="checkbox" name="" id="plane1" onChange={handleNegated} />
+          <label htmlFor="plane1">Negated</label>
+        </span>
+      )}
+    </>
+  );
+}
--- client/src/components/Import/Import.js
+++ client/src/components/Import/Import.js
@@ -1,133 +1,130 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { setSceneModified, getChildrens, getGroup, getSceneModified, getScene } from '../../features/scene/sceneSlice';
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
-import * as THREE from 'three';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { setSceneModified, getChildrens, getGroup, getSceneModified, getScene } from "../../features/scene/sceneSlice";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader.js";
+import * as THREE from "three";
+import { useDispatch } from "react-redux";
 
 export default function Import() {
-	const scene = useSelector(getScene);
-	const group = useSelector(getGroup);
-	const isModified = useSelector(getSceneModified);
-	const dispatch = useDispatch();
+  const scene = useSelector(getScene);
+  const group = useSelector(getGroup);
+  const isModified = useSelector(getSceneModified);
+  const dispatch = useDispatch();
 
-	const [ mesh, setMesh ] = useState();
-	const [ vector, setVector ] = useState(new THREE.Vector3());
-	const [ isFirstImport, setIsFirstImport ] = useState(true);
-	const [ files, setFiles ] = useState([]);
+  const [mesh, setMesh] = useState();
+  const [vector, setVector] = useState(new THREE.Vector3());
+  const [isFirstImport, setIsFirstImport] = useState(true);
+  const [files, setFiles] = useState([]);
 
-	useEffect(
-		() => {
-			if (mesh !== undefined) {
-				// If it is the first file, calculates the position of the mesh otherwise create a group of mesh
-				if (isFirstImport) {
-					setFirstObjPosition(mesh);
-				} else {
-					addMeshToGroup(mesh);
-				}
-			}
-		},
-		[ mesh, vector ]
-	);
+  useEffect(() => {
+    if (mesh !== undefined) {
+      // If it is the first file, calculates the position of the mesh otherwise create a group of mesh
+      if (isFirstImport) {
+        setFirstObjPosition(mesh);
+      } else {
+        addMeshToGroup(mesh);
+      }
+    }
+  }, [mesh, vector]);
 
-	useEffect(
-		() => {
-			if (files.length > 0) {
-				for (var i = 0; i < files.length; i++) {
-					loadFile(files[i]);
-				}
-			}
-		},
-		[ files ]
-	);
+  useEffect(() => {
+    if (files.length > 0) {
+      for (var i = 0; i < files.length; i++) {
+        loadFile(files[i]);
+      }
+    }
+  }, [files]);
 
-	const handleChange = (e) => {
-		const files = e.target.files;
+  const handleChange = (e) => {
+    const files = e.target.files;
 
-		setFiles(files);
-	};
+    setFiles(files);
+  };
 
-	/**
-     * Loads the file and reads the content of the file
-     * @param {File} file File imported
-     */
-	const loadFile = (file) => {
-		const filename = file.name;
-		const reader = new FileReader();
+  /**
+   * Loads the file and reads the content of the file
+   * @param {File} file File imported
+   */
+  const loadFile = (file) => {
+    const filename = file.name;
+    const reader = new FileReader();
 
-		reader.addEventListener(
-			'load',
-			(event) => {
-				const contents = event.target.result;
+    reader.addEventListener(
+      "load",
+      (event) => {
+        const contents = event.target.result;
 
-				setMesh(createMeshFromFile(filename, contents));
-			},
-			false
-		);
+        setMesh(createMeshFromFile(filename, contents));
+      },
+      false
+    );
 
-		reader.readAsArrayBuffer(file);
-	};
+    reader.readAsArrayBuffer(file);
+  };
 
-	/**
-     * Creates a mesh from file
-     * @param {FileName} filename The name of the file
-     * @param {FileReader} contents Result of FileReader
-     * @returns mesh
-     */
-	const createMeshFromFile = (filename, contents) => {
-		const geometry = new STLLoader().parse(contents);
-		//geometry.center();
-		geometry.computeVertexNormals();
+  /**
+   * Creates a mesh from file
+   * @param {FileName} filename The name of the file
+   * @param {FileReader} contents Result of FileReader
+   * @returns mesh
+   */
+  const createMeshFromFile = (filename, contents) => {
+    const geometry = new STLLoader().parse(contents);
+    //geometry.center();
+    geometry.computeVertexNormals();
 
-		const material = new THREE.MeshStandardMaterial({
-			color: '#' + (((1 << 24) * Math.random()) | 0).toString(16)
-		});
+    const material = new THREE.MeshStandardMaterial({
+      color: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+      side: THREE.DoubleSide,
+    });
 
-		const mesh = new THREE.Mesh(geometry, material);
-		mesh.name = filename;
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.name = filename;
 
-		return mesh;
-	};
+    return mesh;
+  };
 
-	/**
-     * Adds the mesh to the group
-     * @param {Mesh} mesh THREE.Mesh
-     */
-	const addMeshToGroup = (mesh) => {
-		group.add(mesh);
+  /**
+   * Adds the mesh to the group
+   * @param {Mesh} mesh THREE.Mesh
+   */
+  const addMeshToGroup = (mesh) => {
+    group.add(mesh);
 
-		setGroupPosition();
-	};
+    setGroupPosition();
+  };
 
-	/**
-     * Sets group position
-     */
-	const setGroupPosition = () => {
-		group.position.set(-vector.x, -vector.y, -vector.z);
+  /**
+   * Sets group position
+   */
+  const setGroupPosition = () => {
+    group.position.set(-vector.x, -vector.y, -vector.z);
 
-		dispatch(setSceneModified(!isModified));
-	};
+    dispatch(setSceneModified(!isModified));
+  };
 
-	/**
-     * Sets the position of the first object of the group
-     * @param {Mesh} mesh THREE.Mesh
-     */
-	const setFirstObjPosition = (mesh) => {
-		const center = new THREE.Vector3();
+  /**
+   * Sets the position of the first object of the group
+   * @param {Mesh} mesh THREE.Mesh
+   */
+  const setFirstObjPosition = (mesh) => {
+    const center = new THREE.Vector3();
 
-		const box3 = new THREE.Box3().setFromObject(mesh);
-		box3.getCenter(center);
+    const box3 = new THREE.Box3().setFromObject(mesh);
+    box3.getCenter(center);
 
-		setVector(center);
-		setIsFirstImport(false);
+    setVector(center);
+    setIsFirstImport(false);
 
-		addMeshToGroup(mesh);
-	};
+    addMeshToGroup(mesh);
+  };
 
-	return (
-		<div>
-			<label htmlFor="input_import" className="btn">Import</label>
-			<input type="file" multiple id="input_import" accept=".stl" onChange={handleChange} />
-		</div>
-	);
+  return (
+    <div>
+      <label htmlFor="input_import" className="btn">
+        Import
+      </label>
+      <input type="file" multiple id="input_import" accept=".stl" onChange={handleChange} />
+    </div>
+  );
 }
