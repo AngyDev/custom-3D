c5f7a5857ec52016c7aaf73a493594678aa0cae7
Angela Busato <busato.angela@gmail.com>
2022-04-05 09:13:19 +0200

    feat: add varibles style and import style in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 
 import store from "./redux/store";
 import App from "./features/App/App";
-import "./assets/styles/index.scss";
+import "./assets/styles/index.css";
 
 ReactDOM.render(
   <Provider store={store}>
