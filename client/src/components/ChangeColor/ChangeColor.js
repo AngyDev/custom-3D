a32d7caee4de19fd151c86efe695791a00af3232
Angela Busato <busato.angela@gmail.com>
2022-05-19 09:49:23 +0200

    feat: add new color

--- client/src/components/ChangeColor/ChangeColor.js
+++ client/src/components/ChangeColor/ChangeColor.js
@@ -14,11 +14,15 @@ export default function ChangeColor({ onClick, onClose }) {
       <div className="modal__body flex color__container text-black">
         <div className="flex flex-col items-center color">
           <input type="button" className="color__box color__yellow color__btn" onClick={(e) => setColor(e.target.name)} name="#fcba03" />
-          <span>Tumore</span>
+          <span className="text-gray-500 dark:text-gray-400">Tumore</span>
         </div>
         <div className="flex flex-col items-center color">
           <input type="button" className="color__box color__blue color__btn" onClick={(e) => setColor(e.target.name)} name="blue" />
-          <span>Osso</span>
+          <span className="text-gray-500 dark:text-gray-400">Osso</span>
+        </div>
+        <div className="flex flex-col items-center color">
+          <input type="button" className="color__box color__red color__btn" onClick={(e) => setColor(e.target.name)} name="red" />
+          <span className="text-gray-500 dark:text-gray-400">Arterie</span>
         </div>
       </div>
       <div className="modal__footer modal__border-t">
