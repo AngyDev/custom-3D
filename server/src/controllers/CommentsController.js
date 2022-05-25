1b6f4af8c80e641dcb762473a20c7504e47d5ede
Angela Busato <busato.angela@gmail.com>
2022-05-25 14:55:42 +0200

    feat: refactor user api

--- server/src/controllers/CommentsController.js
+++ server/src/controllers/CommentsController.js
@@ -1,46 +1,51 @@
-import { CommentsModel } from '../models/CommentsModel';
-const { v4 } = require('uuid');
+import { CommentsModel } from "../models/CommentsModel";
+const { v4 } = require("uuid");
 
 export class CommentsController {
+  /**
+   * Get comments by project id
+   * @param {String} projectId Project ID
+   * @returns list of comments by project id
+   */
+  static getCommentsByProjectId(projectId) {
+    return CommentsModel.query().select("comments.*").where("comments.project_id", projectId);
+  }
 
-    /**
-     * Get comments by project id
-     * @param {String} projectId Project ID
-     * @returns list of comments by project id 
-     */
-    static getCommentsByProjectId(projectId) {
-        return CommentsModel.query().select('comments.*').where('comments.project_id', projectId);
-    }
+  /**
+   * Get comments by projectId and pointId
+   * @param {String} projectId Project ID
+   * @param {String} pointId Point ID
+   * @returns List of comments by projectsId and pointId
+   */
+  static getCommentsByProjectIdAndPointId(projectId, pointId) {
+    return CommentsModel.query()
+      .select(["comments.*", "users.first_name", "users.last_name"])
+      .join("users", "users.id", "comments.user_id")
+      .where("comments.project_id", projectId)
+      .where("comments.point_id", pointId)
+      .orderBy("created_at", "ASC");
+  }
 
-    /**
-     * Get comments by projectId and pointId
-     * @param {String} projectId Project ID
-     * @param {String} pointId Point ID
-     * @returns List of comments by projectsId and pointId
-     */
-    static getCommentsByProjectIdAndPointId(projectId, pointId) {
-        return CommentsModel
-            .query()
-            .select(['comments.*', 'users.first_name', 'users.last_name'])
-            .join('users', 'users.id', 'comments.user_id')
-            .where('comments.project_id', projectId)
-            .where('comments.point_id', pointId)
-            .orderBy("created_at", "ASC");
-    }
+  /**
+   * Insert the comment in the DB
+   * @param {Object} comment
+   * @returns The comment inserted
+   */
+  static createComment(comment) {
+    return CommentsModel.query().insert({
+      id: v4(),
+      text: comment.text,
+      project_id: comment.projectId,
+      user_id: comment.userId,
+      point_id: comment.pointId,
+    });
+  }
 
-    /**
-     * Insert the comment in the DB
-     * @param {Object} comment 
-     * @returns The comment inserted
-     */
-    static createComment(comment) {
-        return CommentsModel.query().insert({
-            id: v4(),
-            text: comment.text,
-            project_id: comment.project_id,
-            user_id: comment.user_id,
-            point_id: comment.point_id
-        })
-    }
-
+  /**
+   * Delete the comment by id
+   * @param {String} id 
+   */
+  static deleteComment(id) {
+    return CommentsModel.query().deleteById(id);
+  }
 }
--- server/src/routes/comments.js
+++ server/src/routes/comments.js
@@ -1,53 +1,9 @@
 import express from 'express';
-import { CommentsController } from '../controllers/CommentsController';
-import { ObjectsController } from '../controllers/ObjectsController';
-import { ProjectsController } from '../controllers/ProjectsController';
-import { UsersController } from '../controllers/UsersControllers';
+const api = require("../api/comments");
 
 const router = express.Router();
 
-/**
- * Get comments by project id and point id
- */
-router.get('/comments/:projectId/:pointId', async(req, res) => {
-    try {
-        const projectId = req.params.projectId;
-        const pointId = req.params.pointId;
-
-        const response = await CommentsController.getCommentsByProjectIdAndPointId(projectId, pointId);
-
-        if (response) {
-            res.send(response);
-        } else {
-            res.status(404).send('Not found');
-        }
-
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-/**
- * Save comment
- */
-router.post('/comment', async(req, res) => {
-    try {
-        const findUser = UsersController.getUserById(req.body.user_id);
-        const findProject = ProjectsController.getProjectById(req.body.project_id);
-        const findObject = ObjectsController.getObjectById(req.body.object_id);
-
-        if (!findUser) return res.status(404).send('User not found');
-        if (!findProject) return res.status(404).send('Project not found');
-        if (!findObject) return res.status(404).send('Object not found');
-
-        const createComment = await CommentsController.createComment(req.body);
-        const response = await CommentsController.getCommentsByProjectIdAndPointId(req.body.project_id, req.body.point_id);
-        return res.status(200).json(response);
-
-    } catch (error) {
-        res.status(400).json(error.message);
-        console.error(error);
-    }
-})
+router.get('/comments/:projectId/:pointId', api.getCommentsByProjectIdAndPointId);
+router.post('/comment', api.createComment);
 
 module.exports = router;
