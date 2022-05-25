9c24d2ad37555637029505c6a2696a858b10f41d
Angela Busato <busato.angela@gmail.com>
2022-05-25 12:49:27 +0200

    feat: add validation

---
+++ server/src/validation/validate.js
@@ -0,0 +1,16 @@
+const { HttpError } = require("../error");
+
+const validateUser = (firstName, lastName, email, password, role) => {
+  if (!(firstName && lastName && email && password && role)) {
+    throw new HttpError(400, "Missing required fields");
+  }
+}
+
+const validateProject = (project) => {
+  console.log(project);
+  if (!(project.projectName && project.patientCode && project.userId)) {
+    throw new HttpError(400, "Missing required fields");
+  }
+}
+
+module.exports = { validateUser, validateProject };
