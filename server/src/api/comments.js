e3bfe201c5f04c327a8234078e4005dc2e08d8fa
Angela Busato <busato.angela@gmail.com>
2022-05-25 14:56:11 +0200

    server/src/api/comments.js

---
+++ server/src/api/comments.js
@@ -0,0 +1,45 @@
+const { errorHandler } = require("../utils");
+const { HttpError } = require("../error");
+const { CommentsController } = require("../controllers/CommentsController");
+const { ObjectsController } = require("../controllers/ObjectsController");
+const { ProjectsController } = require("../controllers/ProjectsController");
+
+/**
+ * Get comments by project id and point id
+ */
+const getCommentsByProjectIdAndPointId = errorHandler(async (req, res) => {
+  const { projectId, pointId } = req.params;
+
+  const comments = await CommentsController.getCommentsByProjectIdAndPointId(projectId, pointId);
+
+  return comments;
+});
+
+/**
+ * Save comment
+ */
+const createComment = errorHandler(async (req, res) => {
+  const { userId } = req.body;
+  const { projectId } = req.body;
+  const { pointId } = req.body;
+
+  const findUser = UsersController.getUserById(userId);
+  const findProject = ProjectsController.getProjectById(projectId);
+  // Cheacks if the object exists in the project
+  const objects = await ObjectsController.getObjectsByProjectId(projectId);
+  const object = objects.find((object) => object.id === pointId);
+
+  if (!findUser) throw new HttpError(404, "User not found");
+  if (!findProject) throw new HttpError(404, "Project not found");
+  if (!object) throw new HttpError(404, "Object not found");
+
+  const createComment = await CommentsController.createComment(req.body);
+
+  if (!createComment) throw new HttpError(400, "Comment not created");
+
+  const comments = await CommentsController.getCommentsByProjectIdAndPointId(projectId, pointId);
+
+  return comments;
+});
+
+module.exports = { getCommentsByProjectIdAndPointId, createComment };
