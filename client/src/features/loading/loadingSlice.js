f29358158f841345915703f51022576084117b89
Angela Busato <busato.angela@gmail.com>
2022-06-15 10:45:22 +0200

    feat: add loading store to App

---
+++ client/src/features/loading/loadingSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const loadingSlice = createSlice({
+  name: "loading",
+  initialState: {
+    loading: false,
+  },
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+  },
+});
+
+export const { setLoading } = loadingSlice.actions;
+
+export const getLoading = (state) => state.loading.loading;
+
+export const loadingReducer = loadingSlice.reducer;
