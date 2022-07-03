b4ef557ea60b38b8c5c70e71f0c4abc48fab3132
Angela Busato <busato.angela@gmail.com>
2022-07-03 10:44:20 +0200

    feat: add download plane info button

--- client/src/components/Panel/PanelPlaneInfo/PanelPlaneInfo.js
+++ client/src/components/Panel/PanelPlaneInfo/PanelPlaneInfo.js
@@ -1,14 +1,30 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { getCenter, getNormal } from "../../../utils/functions/objectCalc";
+import uploadIcon from "../../../assets/images/icons/white/upload-solid.svg";
+import { downloadObject } from "../../../utils/functions/downloadObject";
 
 export default function PanelPlaneInfo({ plane }) {
   const center = getCenter(plane);
   const normal = getNormal(plane);
 
+  const text = `Center: ${center.x.toFixed(3)}, ${center.y.toFixed(3)}, ${center.z.toFixed(3)}\nNormal: ${normal.x.toFixed(3)}, ${normal.y.toFixed(
+    3,
+  )}, ${normal.z.toFixed(3)}`;
+
+  const downloadInfo = () => {
+    const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
+    downloadObject(blob, plane.name + ".txt");
+  };
+
   return (
     <div className="inline-block absolute left-80 z-10 py-2 px-3 text-sm font-medium text-white rounded-lg shadow-sm bg-gray-600 dark:bg-base">
-      <h3>{plane.name} Info</h3>
+      <div className="flex justify-between">
+        <h3>{plane.name} Info</h3>
+        <div>
+          <img className="w-4 h-4 cursor-pointer" src={uploadIcon} alt="Download info" onClick={downloadInfo} />
+        </div>
+      </div>
       <div className="flex flex-col">
         <div className="flex gap-2">
           <span>Normal: </span>
