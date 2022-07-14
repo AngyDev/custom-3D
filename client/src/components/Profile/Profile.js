d227837bf70882f23120aace6340852793a23429
Angela Busato <busato.angela@gmail.com>
2022-07-14 16:20:59 +0200

    feat: create Profile component

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useAuth } from "../../context/AuthContext";
+import { dispatchError } from "../../features/error/errorSlice";
+import { getLogout } from "../../services/api";
+import Dropdown from "../Dropdown/Dropdown";
+
+export default function Profile() {
+  const { user, logout } = useAuth();
+  const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const changePassword = () => {};
+
+  const logoutUser = () => {
+    getLogout()
+      .then(() => logout())
+      .catch((error) => dispatch(dispatchError(error)));
+  };
+
+  return (
+    <Dropdown
+      open={open}
+      setOpen={() => setOpen(!open)}
+      text={`${user.firstName} ${user.lastName}`}
+      options={[
+        { text: "Change Password", onClick: changePassword },
+        { text: "Logout", onClick: logoutUser },
+      ]}
+    />
+  );
+}
