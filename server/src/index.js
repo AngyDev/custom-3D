8d6df491a8ad1d93a3ccd67223fb81ebd3c31088
Angela Busato <busato.angela@gmail.com>
2022-08-09 11:48:35 +0200

    fix: change nginx config read public folder

--- server/src/index.js
+++ server/src/index.js
@@ -45,7 +45,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
