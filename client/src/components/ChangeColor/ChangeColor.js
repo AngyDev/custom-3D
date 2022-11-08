0ca9d2136c1312c38d6adf671d98a8e0831ef1d2
Angela Busato <busato.angela@gmail.com>
2022-11-08 11:31:46 +0100

    fix: color name

--- client/src/components/ChangeColor/ChangeColor.js
+++ client/src/components/ChangeColor/ChangeColor.js
@@ -14,15 +14,15 @@ export default function ChangeColor({ onClick, onClose }) {
       <div className="modal__body flex color__container text-black">
         <div className="flex flex-col items-center color">
           <input type="button" className="color__box color__yellow color__btn" onClick={(e) => setColor(e.target.name)} name="#fcba03" />
-          <span className="text-gray-500 dark:text-gray-400">Tumore</span>
+          <span className="text-gray-500 dark:text-gray-400">Tumor</span>
         </div>
         <div className="flex flex-col items-center color">
           <input type="button" className="color__box color__blue color__btn" onClick={(e) => setColor(e.target.name)} name="#C7AC96" />
-          <span className="text-gray-500 dark:text-gray-400">Osso</span>
+          <span className="text-gray-500 dark:text-gray-400">Bone</span>
         </div>
         <div className="flex flex-col items-center color">
           <input type="button" className="color__box color__red color__btn" onClick={(e) => setColor(e.target.name)} name="red" />
-          <span className="text-gray-500 dark:text-gray-400">Arterie</span>
+          <span className="text-gray-500 dark:text-gray-400">Artery</span>
         </div>
       </div>
       <div className="modal__footer modal__border-t">
