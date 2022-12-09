e3857b8c5b34b54b7a5e02eef29d3c8d7317240a
Angela Busato <busato.angela@gmail.com>
2022-12-09 10:47:15 +0100

    feat: add LayoutEditor component comment

--- client/src/components/Layout/LayoutEditor.js
+++ client/src/components/Layout/LayoutEditor.js
@@ -10,6 +10,11 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Sidebar from "../Sidebar/Sidebar";
 
+/**
+ * This component is the container for the editor page, it contains the header, the left
+ * and right sidebar and the main component with the 3D scene
+ * @returns The editor page
+ */
 export default function LayoutEditor() {
   const { id } = useParams();
   const { project, fetchGetProjectById } = useGetProjectById();
