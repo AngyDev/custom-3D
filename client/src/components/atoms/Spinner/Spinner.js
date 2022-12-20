1bca51a741729686ee76d59ddb90a6eec0ec5257
Angela Busato <busato.angela@gmail.com>
2022-12-20 15:05:09 +0100

    fix: spinner color

--- client/src/components/atoms/Spinner/Spinner.js
+++ client/src/components/atoms/Spinner/Spinner.js
@@ -2,10 +2,11 @@ import React from "react";
 import { MutatingDots } from "react-loader-spinner";
 
 export default function Spinner() {
+  // 191e2a
   return (
     <div className="resultLoader">
       <div className="loader">
-        <MutatingDots ariaLabel="loading-indicator" height={100} width={100} color="#191e2a" secondaryColor="#191e2a" />
+        <MutatingDots ariaLabel="loading-indicator" height={100} width={100} color="#c98a6b" secondaryColor="#c98a6b" />
       </div>
     </div>
   );
