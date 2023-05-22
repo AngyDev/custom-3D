26352ba4ab627876b94bf9cbdea021729843298f
Angela Busato <busato.angela@gmail.com>
2023-05-22 10:28:48 +0200

    feat: add Button test

--- client/src/components/atoms/Button/Button.test.js
+++ client/src/components/atoms/Button/Button.test.js
@@ -1,16 +1,35 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 
 import Button from "./Button";
 
-describe("Button testing", () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = mount(<Button text="Click" />);
+describe("Button component", () => {
+  test("Renders the button component without crashing", () => {
+    render(<Button />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
 
-  it("should contain Button text", () => {
-    expect(wrapper.find("button").text()).toContain("Click");
+  test("Renders the button with props", () => {
+    render(<Button text="Click" type="button" disabled={true} />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toHaveTextContent("Click");
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+
+  test("Renders the button with image", () => {
+    render(<Button text="Click" type="button" img="img" title="image" />);
+
+    const image = screen.getByAltText("image");
+
+    expect(image).toHaveAttribute("src", "img");
+  });
+
+  test("matches snapshot", () => {
+    const tree = renderer.create(<Button text="Click" type="button" img="img" title="image" />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
