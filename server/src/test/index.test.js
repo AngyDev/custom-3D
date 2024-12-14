af2a8a4185c011d1d3cea10e0973969ed5ea0710
Angela Busato <busato.angela@gmail.com>
2024-12-14 17:53:31 +0000

    update dependencies

--- server/src/test/index.test.js
+++ server/src/test/index.test.js
@@ -1,15 +1,13 @@
 const expect = require("chai").expect;
-const request = require("request");
+const axios = require("axios");
 
 const baseUrl = "http://localhost:3000";
 
 describe("Server up", () => {
   const url = baseUrl + "/";
 
-  it("returns status 200", (done) => {
-    request.get(url, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
+  it("returns status 200", async () => {
+    const response = await axios.get(url);
+    expect(response.status).to.equal(200);
   });
 });
